6165d381638bf2dafe78be0100fe9314663890c9
Nytezerak <chrysdragon.97@hotmail.com>
2022-03-11 21:47:29 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import data from "data.json"
+
 import './App.css';
+import Header from './header';
+
 
 function App() {
+  const[toDoList, setToDoList] = useState(data)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
     </div>
   );
 }
