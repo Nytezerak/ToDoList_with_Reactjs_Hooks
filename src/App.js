482b54bfdc25ff62e39400dc540efb8f69db7077
Nytezerak <chrysdragon.97@hotmail.com>
2022-03-11 22:34:59 -0300

    Inserting toggle function

--- src/App.js
+++ src/App.js
@@ -8,6 +8,13 @@ import ToDoList from './ToDoList.js';
 function App() {
   const[toDoList, setToDoList] = useState(data)
 
+  const handleToggle = (id) =>{
+    let mapped = toDoList.map(task => {
+      return task.id == id ? { ...task, complete: !task.complete}:{}
+    })
+    setToDoList(mapped)
+  }
+
   return (
     <div className="App">
       <Header/>
